a40117d9141cec68249ba96f13a5b642a0404cb3
abubala <abubakar.bala@whogohost.com>
2021-04-08 10:53:22 +0100

    Completed hero section :white_check_mark:

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,8 +3,11 @@ import hero from './hero.jpg'
 
 function Hero() {
     return(
-    <div className="container bg-primary hero-image flex center">
+    <div className="container bg-primary hero-image flex flex-col center ">
         <img src={hero} alt="hero image" height={300} width={448} className="rounded"/>
+        <h1 className="hero-title bold mb-0">What Kind Of Developer You?</h1>
+        <p className="lead-text">Take this quiz to find out what kind of developer you are!</p>
+        <button className="btn blue-dark white-text p-4">Take this Quiz</button>
     </div>
     )
 }
