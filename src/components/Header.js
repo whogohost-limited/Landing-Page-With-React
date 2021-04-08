ec91df61f6954250d0d7d5d5ad569e39d3a426a4
abubala <abubakar.bala@whogohost.com>
2021-04-08 09:18:27 +0100

    Added top menu css styles :art:

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 function Header() {
     return(
-        <div className="Flex Space-between">
-            <li className="Float-right">Logo</li>
-            <nav className="Menu-items Flex">
-                <li>Save</li>
-                <li>Invest</li>
-                <li>Stories</li>
-                <li>FAQs</li>
-                <li>Login</li>
+        <div className="menu flex space-between">
+            <li className="float-right"><a href="#">Logo</a></li>
+            <nav className="menu-items flex">
+                <li><a href="#">Save</a></li>
+                <li><a href="#">Invest</a></li>
+                <li><a href="#">Stories</a></li>
+                <li><a href="#">FAQs</a></li>
+                <li><a href="#">Login</a></li>
+                <li className="btn blue-dark"><a href="#" className="white-text">Create a Free Account</a></li>
             </nav>
         </div>
     )
