0659c691b04075081d35ae0ae5b97d56963297fa
abubala <abubakar.bala@whogohost.com>
2021-04-14 08:55:53 +0100

    Changed some functional components to class components :hammer:

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ class Header extends React.Component {
                 </li>
                 <nav className="menu-items flex">
                     <li><a href="#">Save</a></li>
-                    <li><a href="#">Invest</a></li>
+                    <li><a href="#">Invests</a></li>
                     <li><a href="#">Stories</a></li>
                     <li><a href="#">FAQs</a></li>
                     <li><a href="#">Login</a></li>
